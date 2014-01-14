47502114a0d6b17a3e239499eecd77392e0303f0
Greg McGlynn <gem2128@columbia.edu>
2014-01-13 21:04:23 -0500

    Added sprint bot with Debug statements removed.
    Does decently in tests, may optimize noise tower herding.

---
+++ teams/sprint/BotPastr.java
@@ -0,0 +1,13 @@
+package sprint;
+
+import battlecode.common.*;
+
+public class BotPastr extends Bot {
+	public BotPastr(RobotController theRC) {
+		super(theRC);
+	}
+
+	public void turn() throws GameActionException {
+	}
+
+}
